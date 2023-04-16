7a4dd859d4ded8e242b1ca016528d9478a95251a
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2023-04-17 00:28:00 +0500

    added Employee

---
+++ src/main/java/com/spring/mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,34 @@
+package com.spring.mvc_hibernate_aop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+}
